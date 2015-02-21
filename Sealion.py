dfd34b20133f8d50276468d6788df17662a8d5a2
sarahkh13 <sarahkh13@users.noreply.github.com>
2015-02-21 00:01:33 -0800

    Create Sealion.py

---
+++ Sealion.py
@@ -0,0 +1,39 @@
+
+
+from __future__ import division
+#Must include since terminal is python 2, not 3
+
+import re
+
+file1 = open("Vet ACE Patient Data Jan 3 07.txt").readlines()
+newfile =  open("newfile.txt", "w")
+
+	for line in file1: 
+		if match = re.search(r"SEX", line):
+			print match
+		else break	
+	for line in file: 
+		if re.search(r"PATIENT NAME", line)
+    		print line
+ 	for line in file: 
+		if re.search(r"PATIENT ID", line):
+			print line
+	for line in file: 
+		if re.search(r"DRAWN", line):
+			print line
+	for line in file: 
+		if re.search(r"PHOS", line):
+			print line
+	for line in file: 
+		if re.search(r"BUN", line):
+			print line
+	for line in file: 
+		if re.search(r"CREAT", line):
+			print line
+
+	newfile.write(lines)
+	newfile.close()
+
+
+
+	
